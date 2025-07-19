bf29d2396fc7b6d097d51ac8b451d576f9fb6147
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2025-07-19 16:10:15 +0900

    自己紹介文作成

---
+++ about-me-ts/src/components/About.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About:React.FC = () => {
+    return(
+        <div style={{padding:'2rem',fontFamily:'sans-serif'}}>
+            <h2>こんにちは！尾野響と申します！</h2>
+            <p>
+                バックエンド・フロントエンドどちらも挑戦中で、特にNext.jsやGraphQLなど、モダンな技術の習得を目指しております。
+            </p>
+            <p>
+                このポートフォリオサイトでは、私が作ったアプリや学習記録を発信していきます。どうぞよろしくお願いいたします。
+            </p>
+        </div>
+    );
+};
+
+export default About;
