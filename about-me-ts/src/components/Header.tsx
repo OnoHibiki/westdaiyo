4f26a92c707e7f14316bc09d452c06a55519baf0
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2025-07-19 16:23:36 +0900

    Header追加

---
+++ about-me-ts/src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header: React.FC = () => {
+    return(
+        <header style={{ background: '#222', color:'#fff', padding: '1rem'}}>
+            <h1>Hibiki'sのポートフォリオサイト</h1>
+        </header>
+    );
+};
+
+export default Header;
