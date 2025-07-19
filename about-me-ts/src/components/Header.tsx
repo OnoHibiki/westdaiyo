9cf32b98307340546ba16e48d4cb4df2f8ef2367
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2025-07-19 16:53:50 +0900

    7/19

--- about-me-ts/src/components/Header.tsx
+++ about-me-ts/src/components/Header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header: React.FC = () => {
     return(
         <header style={{ background: '#222', color:'#fff', padding: '1rem'}}>
-            <h1>Hibiki'sのポートフォリオサイト</h1>
+            <h1 style={{marginTop:0}}>Hibiki's ポートフォリオサイト</h1>
         </header>
     );
 };
--- about-me-ts/src/components/Layout.tsx
+++ about-me-ts/src/components/Layout.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "./Header";
-import Footer from "./Footter";
+import Footer from "./Footer";
 
 type LayoutProps = {
     children: React.ReactNode;
