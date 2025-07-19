e52bec31eb8de910508c16211a9a4aede87e0bb3
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2025-07-19 16:28:44 +0900

    Footer追加

---
+++ about-me-ts/src/components/Footter.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer: React.FC =() => {
+    return(
+        <footer style={{background: '#eee', color: '#333', padding: '1rem',marginTop:'2rem'}}>
+            <p>©️ 2025 Hibiki Ono</p>
+        </footer>
+
+    );
+};
+
+export default Footer;
