356ad8722b05350bf3685f65520535060d9cf2f8
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2025-07-20 22:29:27 +0900

    CSS追加

--- about-me-ts/src/App.tsx
+++ about-me-ts/src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
+import Layout from './components/Layout';
 import About from './components/About';
 
 function App(){
   return(
-    <div>
-        <h1>ポートフォリサイトへようこそ</h1>
+    <Layout>
         <About />
-    </div>
+    </Layout>
   );
 }
 
