af4f9e6c68f142808a087583ab9e9ced43cf01da
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2025-07-27 20:16:11 +0900

    WorkCard追加

--- about-me-ts/src/components/About.tsx
+++ about-me-ts/src/components/About.tsx
@@ -4,6 +4,8 @@ import profile1 from "../assets/profile1.png";
 import profile2 from "../assets/profile2.jpg";
 import profile3 from "../assets/profile3.png";
 
+import WorkCard from "./WorkCard";
+
 const About:React.FC = () => {
     //複数の画像を配列で管理。ランダム表示
     const images =[profile1,profile2,profile3];
@@ -13,25 +15,36 @@ const About:React.FC = () => {
         return images[randomIndex];
     },[]);
 
-    return(
-        <div className={styles.container}>
-            <img src={selectedImage} alt="プロフィール画像" className={styles.image} />
-            
-            <div className={styles.textArea}>
-                <h2 className={styles.heading}>こんにちは！尾野響と申します！</h2>
-                <p className={styles.paragraph}>
-                    バックエンド・フロントエンドどちらも挑戦中で、<br />
-                    特にNext.jsやGraphQLなど、<br />
-                    モダンな技術の習得を目指しております。
-                </p>
-                <p className={styles.paragraph}>
-                    このポートフォリオサイトでは、<br />
-                    私が作ったアプリや学習記録を発信していきます。<br />
-                    どうぞよろしくお願いいたします。
-                </p>
+    return (
+        <div>
+            <div className={styles.profileSection}>
+
+                <div className={styles.container}>
+                    <img src={selectedImage} alt="プロフィール画像" className={styles.image} />
+
+                    <div className={styles.textArea}>
+                        <h2 className={styles.heading}>こんにちは！尾野響と申します！</h2>
+                        <p className={styles.paragraph}>
+                            バックエンド・フロントエンドどちらも挑戦中で、<br />
+                            特にNext.jsやGraphQLなど、<br />
+                            モダンな技術の習得を目指しております。
+                        </p>
+                        <p className={styles.paragraph}>
+                            このポートフォリオサイトでは、<br />
+                            私が作ったアプリや学習記録を発信していきます。<br />
+                            どうぞよろしくお願いいたします。
+                        </p>
+                    </div>
+
+                </div>
+                
             </div>
-        
+
+            <WorkCard />
+
+
         </div>
+        
     );
 };
 
