028b75ded4c9201a69ab30e01a166b287645d3d3
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2026-01-31 16:18:29 +0900

    よりシンプルに

--- assets/ts/index.ts
+++ assets/ts/index.ts
@@ -1,11 +1,11 @@
 // index.htmlのTOP画像をランダムに回す
 
-window.onload = function() {
+window.onload =  () =>  {
     const TOTAL_IMAGES = 2; // 画像が増えたらここだけ変える
     
     const num = Math.floor(Math.random() * TOTAL_IMAGES) + 1;
     
-    const profileImg = document.querySelector('.profile-img');
+    const profileImg = document.querySelector<HTMLImageElement>('.profile-img');
     if (profileImg) {
         profileImg.src = `/assets/images/profile/${num}.jpg`;
     }
