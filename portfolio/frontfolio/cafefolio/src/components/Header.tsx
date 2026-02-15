e21839f3bcd8015199876b81e1a81dc1983e9e66
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2026-02-15 13:16:09 +0900

    forntfolio追加

---
+++ portfolio/frontfolio/cafefolio/src/components/Header.tsx
@@ -0,0 +1,11 @@
+
+
+import styles from "./Header.module.css";
+
+export function Header() {
+    return(
+        <header className={styles.header}>
+            <h1 className={styles.title}>テスト</h1>
+        </header>
+    );
+}
