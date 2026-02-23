2026533307eb0a6be57b83f49b33c0ce09de8587
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2026-02-23 19:58:28 +0900

    Aboutセクション追加

--- portfolio/frontfolio/cafefolio/src/components/Hero/Hero.tsx
+++ portfolio/frontfolio/cafefolio/src/components/Hero/Hero.tsx
@@ -7,8 +7,8 @@ export function Hero () {
             <img src={heroImage} alt="コーヒー" className={styles.heroImage}  />
             <div className={styles.overlay}></div> {/*背景画像透過用*/}
             <div className={styles.content}>
-                <h1>ゴリラも驚き</h1>
-                <p>先祖返りをしてしまいそうなほどの安らぎ空間</p>
+                <h1>安らぎのカフェ</h1>
+                <p>喧騒とは無縁の時間</p>
             </div>
         </section>
     );
