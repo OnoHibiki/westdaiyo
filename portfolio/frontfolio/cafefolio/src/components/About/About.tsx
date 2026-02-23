fe1e6351e12378d202092f8234e599c95917cedc
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2026-02-23 20:38:30 +0900

    HeroへInnerクラスを作成。基本1200px予定

---
+++ portfolio/frontfolio/cafefolio/src/components/About/About.tsx
@@ -0,0 +1,34 @@
+import styles from "./About.module.css";
+import ownerImg from "../../assets/images/owner.JPG";
+
+export function About() {
+    return(
+        <section id="about" className={styles.about}>
+            <div className={styles.inner}>
+                
+                {/* 左：画像 */}
+                <div className={styles.media}>
+                    <img 
+                        className={styles.image}
+                        src={ownerImg} 
+                        alt="店主の写真" 
+                    />
+                {/* 右：テキスト */}
+                    <div className={styles.text}>
+                        <h2 className={styles.title}>What?</h2>
+                        <p className={styles.lead}>
+                            喫茶Ono 店主・尾野 響
+                        </p>
+                        <p className={styles.description}>
+                            コーヒーの香りと共に、<br />
+                            非日常に少しの余白を。<br />
+                            Since1963の時代の香りと共に、<br />
+                            あなたを、恐怖へ誘う・・・
+                        </p>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+    );
+}
