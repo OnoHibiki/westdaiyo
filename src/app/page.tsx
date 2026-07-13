7ac93dd5fc0ac02119802825e967ba779fcf2c49
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2026-07-13 16:04:14 +0900

    greetセクション、仮置き

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,22 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main>
+    <main className={styles.main}>
       <div className={styles.hero}>
         <img className={styles.heroImg} src="/images/skydiving.webp" alt="優秀な人材" />
         <div className={styles.heroText}>
           <h1>優秀な人材、<br></br>ここにいます</h1>
         </div>
       </div> 
+
+      <div className={styles.greet}>
+        <img className={styles.greetImg} src="/images/greet.png" alt="尾野 響" />
+        <div className={styles.greetText}>
+          <h1>尾野 響</h1>
+          <p>大学卒業後、バンド活動を６年くらいやったよ。
+          <br />今はシステムエンジニアを志しているよ。</p>
+        </div>
+      </div>
     </main>
   );
 }
