fddbc4fc230bc58b3327745c57cd20073086d35d
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2026-07-13 15:27:46 +0900

    page.tsx hero部分完成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <>
-      <main>
-        <h1>こんにちは！</h1>
-        <p>これからどんどんやっていきたいですよね！（Githubアクション確認用)</p>
-      </main>
-    </>
+    <main>
+      <div className={styles.hero}>
+        <img className={styles.heroImg} src="/images/skydiving.webp" alt="優秀な人材" />
+        <div className={styles.heroText}>
+          <h1>優秀な人材、<br></br>ここにいます</h1>
+        </div>
+      </div> 
+    </main>
   );
 }
