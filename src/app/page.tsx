0fc33c61f217e2ac99270e6cb53e362b43d5681b
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2026-07-14 21:56:34 +0900

    Work準備

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,6 +16,19 @@ export default function Home() {
           <h1>尾野 響</h1>
           <p>大学卒業後、バンド活動を６年くらいやったよ。
           <br />今はシステムエンジニアを志しているよ。</p>
+          <a href="">詳細はこちら</a>
+        </div>
+      </div>
+
+      <div className={styles.works}>
+        <div className={`${styles.worksCard} ${styles.frontCard}`}>
+          FrontEnd
+        </div>
+        <div className={`${styles.worksCard} ${styles.backCard}`}>
+          BackEnd
+        </div>
+        <div className={`${styles.worksCard} ${styles.infraCard}`}>
+          Infrastructure
         </div>
       </div>
     </main>
