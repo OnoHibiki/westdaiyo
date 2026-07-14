97c91c5605ff2bbfc7a8ba2125decfe105f0d2df
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2026-07-14 21:38:55 +0900

    greet部分修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <div className={styles.hero}>
         <img className={styles.heroImg} src="/images/skydiving.webp" alt="優秀な人材" />
         <div className={styles.heroText}>
-          <h1>優秀な人材、<br></br>ここにいます</h1>
+          <h1>優秀な人材、<br></br>ここにいます。</h1>
         </div>
       </div> 
 
