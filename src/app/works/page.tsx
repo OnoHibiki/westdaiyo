1a9a4e25916d8193185d8cc07686d23202922762
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2026-07-16 21:43:03 +0900

    Works仮置き

--- src/app/works/page.tsx
+++ src/app/works/page.tsx
@@ -3,7 +3,27 @@ import styles from "./page.module.css";
 export default function WorkPage() {
     return (
         <main className={styles.main}>
-
+            <div className={styles.worksGreet}>
+                <img src="/images/parasailing.JPG" alt="俺やで" />
+                <p className={styles.worksGreetText}>
+                    いろいろな作品を書いていきます<br />
+                    よろしゅうお願いします
+                </p>
+            </div>
+            <div className={styles.workCards}>
+                <div className={styles.frontCard}>
+                    <h2>FrontEnd</h2>
+                    <p>ここに作品書いていく</p>
+                </div>
+                <div className={styles.backCard}>
+                    <h2>BackEnd</h2>
+                    <p>ここに作品書いていく</p>
+                </div>
+                <div className={styles.infraCard}>
+                    <h2>Infrastructure</h2>
+                    <p>ここに作品書いていく</p>
+                </div>
+            </div>
         </main>
     )
 }
