de97dd132010c375092e7d0cf92613dc0a3ba646
Hibiki Ono <173211997+SuperGorillas@users.noreply.github.com>
2026-09-08 21:27:48 +0900

    feat:メニュー項目追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,29 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
+
       <section className={styles.hero}>
+
+        <button className={`${styles.bubble} ${styles.about}`}>
+          About
+        </button>
+
+        <button className={`${styles.bubble} ${styles.works}`}>
+          Works
+        </button>
+
+        <button className={`${styles.bubble} ${styles.contact}`}>
+          CONTACT
+        </button>
+
         <img 
           className={styles.heroImage}
           src="/images/greet.png"
           alt="Hibiki Ono" 
         />
+
       </section>
+
       <footer className={styles.footer}>
         <p>© 2026 Hibiki Ono</p>
       </footer>
